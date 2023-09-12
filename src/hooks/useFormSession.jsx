11c10faf587872577f29ac03bb60c1d86fde2131
luis Colina <colina.1990lz@gmail.com>
2023-09-12 09:23:57 -0400

    hook logic completed

--- src/hooks/useFormSession.jsx
+++ src/hooks/useFormSession.jsx
@@ -1,7 +1,43 @@
-import {useState} from 'react'
-
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 const useFormSession = () => {
-  return {}
-}
+  /* order state */
+  const [order, setOrder] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    selectedPlan: "",
+    billingCycle: false,
+    selectedAddons: [],
+  });
 
-export default useFormSession
+  /* logic of the Form */
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmitForm = (e) => {
+    setOrder((prev) => ({ ...prev, ...e }));
+  };
+
+  /* step controller */
+  const [step, setStep] = useState(0);
+
+  const goToStep = (number) => {
+    let currentStep = step;
+    setStep(number >= 1 && number <= 4 ? number : currentStep);
+  };
+  return {
+    order,
+    handleSubmit,
+    register,
+    errors,
+    onSubmitForm,
+    step,
+    goToStep,
+  };
+};
+
+export default useFormSession;
